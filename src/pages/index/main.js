7cda51708562d146d068301a117f45750799361f
aiym <924718080@qq.com>
2018-06-21 13:47:54 +0800

    update store 注入方式

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import store from '../../store/store'
 import App from './index'
 
 Vue.use(Vuex)
 
-const app = new Vue({
-  ...App,
-  store
-})
+const app = new Vue(App)
 app.$mount()
